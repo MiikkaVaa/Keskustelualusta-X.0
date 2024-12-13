7982fe45bf75bdc6a52dfb39088668f80110daf7
Väänänen Miikka <vmiikka@cubbli24-gold.cs.helsinki.fi>
2024-12-13 22:20:53 +0200

    Create new tabels for threads, messages and permissions

--- schema.sql
+++ schema.sql
@@ -1,13 +1,35 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username TEXT UNIQUE,
-    password TEXT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
     is_admin BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE forums (
     id SERIAL PRIMARY KEY,
-    name TEXT,
-    user_id INTEGER REFERENCES users(id),
+    name TEXT NOT NULL,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
     private BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE threads (
+    id SERIAL PRIMARY KEY,
+    forum_id INTEGER REFERENCES forums(id) ON DELETE CASCADE,
+    message TEXT NOT NULL,
+    user_created INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    time TIMESTAMP DEFAULT NOW
+);
+
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    thread_id INTEGER REFERENCES threads(id) ON DELETE CASCADE,
+    message TEXT NOT NULL,
+    user_created INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    time TIMESTAMP DEFAULT NOW
+);
+
+CREATE TABLE permissions (
+    id SERIAL PRIMARY KEY,
+    forum_id INTEGER REFERENCES forums(id) ON DELETE CASCADE,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
